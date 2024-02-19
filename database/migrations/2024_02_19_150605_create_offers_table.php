aadbd3a7832cf913be77eb1241e749ee02fcd0d0
unknown <aymane.bisdaoune@gmail.com>
2024-02-19 20:22:03 +0100

    migrations done

---
+++ database/migrations/2024_02_19_150605_create_offers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('contract');
+            $table->string('description');
+            $table->string('salary');
+            $table->string('experience');
+            $table->integer('status')->default(1);
+            $table->foreignId('agent_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('offers');
+    }
+};
